10f6affec96b64e740395f4939fe6d24c06d9cb4
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-17 09:10:46 +0000

    Add thymeleaf markups to the links to the images, javaScript files and css styles. Create HomeController to thest modified views.

--- src/main/java/marcykiewicz/mateusz/charitydonationlab/home/HomeController.java
+++ src/main/java/marcykiewicz/mateusz/charitydonationlab/home/HomeController.java
@@ -1,4 +1,28 @@
 package marcykiewicz.mateusz.charitydonationlab.home;
 
-public class HOmeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String displayHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterFrom() {
+        return "register";
+    }
+
+    @GetMapping("/registerConfirmation")
+    public String displayConfirmationForm() {
+        return "form-confirmation";
+    }
 }

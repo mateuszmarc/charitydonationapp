359bb6f9985093518c1160a5f6ada5ecfa838069
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-17 06:34:13 +0000

    Initial commit

---
+++ src/main/java/marcykiewicz/mateusz/charitydonationlab/CharitydonationlabApplication.java
@@ -0,0 +1,13 @@
+package marcykiewicz.mateusz.charitydonationlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitydonationlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitydonationlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marcykiewicz/mateusz/charitydonationlab/CharitydonationlabApplicationTests.java
@@ -0,0 +1,13 @@
+package marcykiewicz.mateusz.charitydonationlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharitydonationlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

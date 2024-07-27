ffa096f2770e7899013c735f5163c0ed3aedb919
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-27 18:41:41 +0000

    Add SecurityFilterChain Bean to AppSecurityConfiguration

--- src/main/java/marcykiewicz/mateusz/charitydonationlab/security/AppSecurityConfig.java
+++ src/main/java/marcykiewicz/mateusz/charitydonationlab/security/AppSecurityConfig.java
@@ -2,9 +2,13 @@ package marcykiewicz.mateusz.charitydonationlab.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +18,27 @@ public class AppSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security, HttpSecurity httpSecurity) throws Exception {
+
+        security
+                .cors(withDefaults())
+                .csrf(configurer -> configurer.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(
+                        "/register/**",
+                        "/login/**",
+                        "/about/**",
+                        "/contact/**",
+                        "/main/**",
+                        "/css/**",
+                        "/images/**"
+                ).permitAll()
+                        .requestMatchers("/users/**").hasAnyAuthority("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
 }
